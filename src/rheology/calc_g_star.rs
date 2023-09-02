e91f415bdeef90725f6121fa6c8dcc4f5000057b
danlindleybaker <danlindleybaker@gmail.com>
2023-09-02 14:24:39 +0100

    problem is in calc_g_star_slow

--- src/rheology/calc_g_star.rs
+++ src/rheology/calc_g_star.rs
@@ -3,15 +3,14 @@ use crate::rheology::g_star_glass::g_star_glass;
 use crate::rheology::g_star_rouse::g_star_rouse;
 use crate::rheology::g_star_slow::g_star_slow;
 use crate::{CLPoly, DataArrays, Parameters, Results};
-use std::io::Write;
 use std::fs::File;
+use std::io::Write;
 pub fn calc_g_star(
     parameters: &mut Parameters,
     lpoly: &mut Vec<CLPoly>,
     data_arrays: &mut DataArrays,
 ) -> Results {
-
-    let mut output_file = File::create("outp.dat").unwrap();  // automatically closed when out of scope
+    let mut output_file = File::create("outp.dat").unwrap(); // automatically closed when out of scope
     let mut results = Results::new();
     parameters.freq_min = parameters.freq_min * parameters.tau_e;
     parameters.freq_max = parameters.freq_max * parameters.tau_e;
@@ -21,7 +20,7 @@ pub fn calc_g_star(
         let mut gp: f64;
         let mut g2p: f64;
         let mut ep: f64 = 0.0;
-        let mut e2p: f64= 0.0;
+        let mut e2p: f64 = 0.0;
 
         let mut gptmp = 0.0;
         let mut g2ptmp = 0.0;
@@ -50,7 +49,7 @@ pub fn calc_g_star(
             g_star_fast_rouse(freq, &mut gptmp, &mut g2ptmp, parameters, lpoly); // In tube Rouse modes
             gp += gptmp;
             g2p += g2ptmp;
-
+// TODO: problem is in g_star_slow
             g_star_slow(
                 freq,
                 &mut gptmp,
@@ -64,17 +63,28 @@ pub fn calc_g_star(
             g2p += (1.0 - parameters.rouse_wt) * g2ptmp;
             ep += (1.0 - parameters.rouse_wt) * eptmp;
             e2p += (1.0 - parameters.rouse_wt) * e2ptmp;
+            println!("gp = {}", gp);
         }
-        
+
         let visc = parameters.g_0 * parameters.tau_e * (gp * gp + g2p * g2p).sqrt() / freq;
-        results.freq.push(freq/parameters.tau_e);
-        results.gp_ar.push(gp*parameters.g_0);
-        results.g2p_ar.push(g2p*parameters.g_0);
+        results.freq.push(freq / parameters.tau_e);
+        results.gp_ar.push(gp * parameters.g_0);
+        results.g2p_ar.push(g2p * parameters.g_0);
         results.ep_ar.push(ep);
         results.e2p_ar.push(e2p);
         results.viscosity_ar.push(visc);
 
-        writeln!(&mut output_file, "{:.6}\t{:.6}\t{:.6}\t{:.6}\t{:.6}\t{:.6}", freq/parameters.tau_e, gp*parameters.g_0, g2p*parameters.g_0, ep, e2p, visc).unwrap();    
+        writeln!(
+            &mut output_file,
+            "{:.6}\t{:.6}\t{:.6}\t{:.6}\t{:.6}\t{:.6}",
+            freq / parameters.tau_e,
+            gp * parameters.g_0,
+            g2p * parameters.g_0,
+            ep,
+            e2p,
+            visc
+        )
+        .unwrap();
     }
-  results
+    results
 }
