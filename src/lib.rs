01988118eececd0cf1c970c52f380b65f2bfafcb
danlindleybaker <danlindleybaker@gmail.com>
2023-09-05 13:18:08 +0100

    added lp2r function to lib.rs so that it can be compiled as .dll -> pyd for python

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,45 @@
 pub mod prep;
 pub mod relax;
 pub mod rheology;
+use crate::prep::init_parameters;
+use crate::relax::time_step;
+use crate::rheology::lin_rheology;
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn lp2r() {
+    let input_file: &str = "inp.dat";
+    println!("LP2R Rust...");
+
+    // parse input file
+    let (mut parameters, mut lpoly, mut data_arrays) =
+        init_parameters(input_file).expect("Problem parsing input file");
+
+    let mut nalive: i32;
+    let mut count = 1;
+    if parameters.entangled_dynamics {
+        nalive = time_step(0, &mut parameters, &mut lpoly, &mut data_arrays);
+
+        while nalive > 0 {
+            nalive = time_step(1, &mut parameters, &mut lpoly, &mut data_arrays);
+            if count == 609 {
+         //       println!("phi_st_ar[69] = {}", data_arrays.phi_st_ar[count]);
+            }
+//            println!("Count = {}", count);
+            count += 1;
+        }
+    }
+
+    let _results = lin_rheology(&mut parameters, &mut lpoly, &mut data_arrays);
+
+    //
+}
+
+#[pymodule]
+fn lp2r_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(lp2r,m)?)?;
+    Ok(())
+}
 
 #[derive(Debug)]
 pub struct CLPoly {
